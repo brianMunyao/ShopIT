8b2326a41cbd0e26d2a8f4fee25eb0bb43dca207
Patrick-Ngige <wakemanjajr@gmail.com>
2023-05-03 21:54:04 +0300

    styling the body structure

--- wp-content/themes/ShopIT/page-product.php
+++ wp-content/themes/ShopIT/page-product.php
@@ -1,5 +1,138 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-<div class="p-4">Test Product page</div>
+<div class="p-3 mb-2 bg-light text-dark ">
+    <div class=" row grid gap-5  ms-4 max-width: 100%; mb-5 ">
+        <div class="col-8 p-3 mb-2 bg-white text-dark border shadow-sm ">
+            <div class="">
+                <div class="small-pics">
+                    <img src="./p-images/pimage.png" alt="aa">
+                    <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" alt="">
+                    <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" alt="">
+                    <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" alt="">
+                </div>
+                <div class="main-pic">
+                    <img src="./p-images/" alt="">
+                </div>
+            </div>
+        </div>
+        <div class=" col-3 p-3 mb-2 bg-white text-dark border shadow-sm w-25 h-50">
+            <div class="description">
+                <p>Brand: Sony</p>
+                <h4>SONY 65" Class CU7000B Crystal <br> UHD 4K UHD Smart TV <br> UN65CUCU7000BXZA 2023</h4>
+                <img src="/ShopIT/p-images/" alt="">
+                <p>KSh 39,799</p>
+                <p class=""> <span class="text-decoration-line-through opacity-50">
+                        KSh 42,999
+                    </span><span>-35%</span></p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
 
-<?php get_footer();?>
+
+    </div>
+
+
+
+
+    <div>
+
+        <div class="col-8 round-1 p-4 mb-2 bg-white text-dark border shadow-sm ms-4 mb-5">
+            <div class="description">
+                <h5>Description</h5>
+                <hr>
+                <p>True-to-life color. Effortless connectivity. Dazzling 4K value. Samsung Crystal UHD is worth a look (and more). Effortlessly access TV shows, movies and ambient content using the Samsung Smart Hub, or find a range of great games on the Samsung Gaming Hub.(High speed internet connection, additional gaming service subscriptions and compatible controller required.) Enjoy content even more clear than it was created as its upgraded to 4K resolution. Color and contrast are also improved with our PurColor and Mega Contrast technologies that instantly analyze and adjust what you see on screen. As all the visual details shine through, you'll be surrounded by 3D sound that moves with the action, engaging all your senses.</p>
+            </div>
+
+            <div class="specification">
+                <h5>Specification</h5>
+                <p>SKU: VI799EA1QMSQ4NAFAMZ
+                    Model: VP8832DF
+                    Production Country: China
+                    Size (L x W x H cm): -
+                    Weight (kg): 3
+                    Main Material: pvc
+                    Shop Type: Jumia Mall
+                </p>
+            </div>
+
+        </div>
+
+        <div class="col-8 round-1 p-3 mb-2 bg-white text-dark border shadow-sm ms-4 mb-5">
+            <div class="col-8">
+                <div class="reviews">
+                    <h5>Customer Reviews</h5>
+                    <hr>
+                    <div class="rating">
+                        <p><span> 5</span> /5 </p>
+                        <div>
+                            <p>5 stars -----------------</p>
+                            <p>4 stars -----------------</p>
+                            <p>3 stars -----------------</p>
+                            <p>2 stars -----------------</p>
+                            <p>1 star ------------------</p>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-8 rounded-1 p-3 mb-2 bg-white text-dark border shadow-sm ms-4 mb-5">
+            <div class="reviews">
+                <h5>Martin M.</h5>
+                <hr>
+                <img src="" alt="">
+                <p>Used fir steaming service quality was excellent I will guarantee it to my friends and family. I am sure they will love it and appreciate it as much as I have enjoyed the experience of being able to try out and enjoying the product. </p>
+            </div>
+        </div>
+
+
+
+        <div class="col-8 rounded-1 p-3 mb-2 bg-white text-dark border shadow-sm ms-4 mb-5">
+            <div class="reviews">
+                <h5>Martin M.</h5>
+                <hr>
+                <img src="" alt="">
+                <p>Used fir steaming service quality was excellent I will guarantee it to my friends and family. I am sure they will love it and appreciate it as much as I have enjoyed the experience of being able to try out and enjoying the product. </p>
+            </div>
+        </div>
+
+
+        <div class=" col-8 p-3 mb-2 bg-white text-dark border shadow-sm ms-4">
+            <h5>You may also like </h5>
+            <hr>
+            <div class="likes d-flex justify-content-center grid gap-5 ">
+
+                <div class="rounded-1 border shadow-sm p-4">
+                    <img src="./p-images/oraimo.jpg" alt="oraimo">
+                    <p>oraimo earpods</p>
+                    <p> KSh. 5,499</p>
+                    <p class="text-decoration-line-through opacity-50"> KSh. 6,499</p>
+                </div>
+                <div class="rounded-1 border shadow-sm p-4">
+                    <img src="./p-images/sony.jpg" alt="">
+                    <p>oraimo earpods</p>
+                    <p> KSh. 5,499</p>
+                    <p class="text-decoration-line-through opacity-50"> KSh. 6,499</p>
+                </div>
+                <div class="rounded-1 border shadow-sm p-4">
+                    <img src="" alt="">
+                    <p>oraimo earpods</p>
+                    <p> KSh. 5,499</p>
+                    <p class="text-decoration-line-through opacity-50"> KSh. 6,499</p>
+                </div>
+                <div class="rounded-1 border shadow-sm p-4">
+                    <img src="" alt="">
+                    <p>oraimo earpods</p>
+                    <p> KSh. 5,499</p>
+                    <p class="text-decoration-line-through opacity-50"> KSh. 6,499</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <?php get_footer(); ?>
