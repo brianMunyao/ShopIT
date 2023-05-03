1b0b4d9c9f9aaeeb71775384eaeaf77385b4a5ce
Patrick-Ngige <wakemanjajr@gmail.com>
2023-05-03 16:37:48 +0300

    tailwindcss -> bootstrapcss

--- wp-content/themes/ShopIT/functions.php
+++ wp-content/themes/ShopIT/functions.php
@@ -1,18 +1,31 @@
 <?php
 
-function shopittheme_script_enqueue(){
-    
+function shopit_theme_script_enqueue(){
+    wp_register_style('bootstrapcss', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', [], '5.2.3', 'all');
+
+    wp_enqueue_style('bootstrapcss');
+
+    wp_register_script('jsbootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js', [], '5.2.3', false);
+    wp_enqueue_script ('jsbootstrap');
 }
 
-add_action('wp_enqueue_scripts', 'shopittheme_script_enqueue');
+add_action('wp_enqueue_scripts', 'shopit_theme_script_enqueue');
 
 // menu and footer
-function shopittheme_setup(){
+function shopit_theme_setup(){
     add_theme_support('menus');
     register_nav_menu('primary', 'Primary Header');
     register_nav_menu('secondary', 'Footer Navigation');
 }
-add_action('init', 'shopittheme_setup');
+add_action('init', 'shopit_theme_setup');
+
+// ADDING NAVWALKER CLASS
+if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
+    return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+} else {
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action('init','shopit_theme_setup');
 
 
 
--- wp-content/themes/ShopIT/header.php
+++ wp-content/themes/ShopIT/header.php
@@ -5,12 +5,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+    <title><?php bloginfo('name');?></title>
     <?php wp_head(); ?>
 </head>
 
 <body>
-    <nav>
-        <?php wp_nav_menu(['theme_location' => 'primary']); ?>
+  
+       <!-- NAVWALKER -->
+    <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1"
+                aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="/shopit/">ShopIT</a>
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'primary',
+                    'depth' => 2,
+                    'container' => 'div',
+                    'container_class' => 'collapse navbar-collapse',
+                    'container_id' => 'bs-example-navbar-collapse-1',
+                    'menu_class' => 'nav navbar-nav',
+                    'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker' => new WP_Bootstrap_Navwalker(),
+                ));
+            ?>
+        </div>
     </nav>
+    
