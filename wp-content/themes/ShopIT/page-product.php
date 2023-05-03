57eef967ab8d9b1a8b0c9d0844706e87f24e8d10
Patrick-Ngige <wakemanjajr@gmail.com>
2023-05-03 23:57:04 +0300

    adding images and further styling

--- wp-content/themes/ShopIT/page-product.php
+++ wp-content/themes/ShopIT/page-product.php
@@ -3,28 +3,40 @@
 <div class="p-3 mb-2 bg-light text-dark ">
     <div class=" row grid gap-5  ms-4 max-width: 100%; mb-5 ">
         <div class="col-8 p-3 mb-2 bg-white text-dark border shadow-sm ">
-            <div class="">
-                <div class="small-pics">
-                    <img src="./p-images/pimage.png" alt="aa">
-                    <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" alt="">
-                    <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" alt="">
-                    <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" alt="">
+            <div class="d-flex flex-row ">
+                <div class="w-25">
+                    <picture class="d-flex flex-column mb-2">
+                        <source srcset="" type="image/svg+xml">
+                        <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" class="img-fluid img-thumbnail w-75 h-75 mb-1" alt="...">
+                        <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" class="img-fluid img-thumbnail w-75 h-75  mb-1" alt="...">
+                        <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" class="img-fluid img-thumbnail w-75 h-75  mb-1" alt="...">
+                        <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" class="img-fluid img-thumbnail w-75 h-75  " alt="...">
+                    </picture>
                 </div>
-                <div class="main-pic">
-                    <img src="./p-images/" alt="">
+                <div class=" w-75">
+                    <img src="https://cdn.pixabay.com/photo/2016/03/21/21/37/tv-1271650__340.png" class="" alt="...">
                 </div>
             </div>
         </div>
         <div class=" col-3 p-3 mb-2 bg-white text-dark border shadow-sm w-25 h-50">
             <div class="description">
                 <p>Brand: Sony</p>
-                <h4>SONY 65" Class CU7000B Crystal <br> UHD 4K UHD Smart TV <br> UN65CUCU7000BXZA 2023</h4>
+                <h6>SONY 65" Class CU7000B Crystal  UHD 4K UHD Smart TV UN65CUCU7000BXZA 2023</h6>
                 <img src="/ShopIT/p-images/" alt="">
-                <p>KSh 39,799</p>
+                <div class="d-flex flex-row gap-row-2 " >
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                </div>
+                <p class="fw-bold" >KSh 39,799</p>
                 <p class=""> <span class="text-decoration-line-through opacity-50">
                         KSh 42,999
-                    </span><span>-35%</span></p>
-                <button>Add To Cart</button>
+                    </span><span class="text-success" > &nbsp; -35% </span></p>
+                <div class="d-grid gap-2">
+                    <button class="btn btn-secondary" type="button">Add to Cart</button>
+                </div>
             </div>
         </div>
 
@@ -39,50 +51,52 @@
         <div class="col-8 round-1 p-4 mb-2 bg-white text-dark border shadow-sm ms-4 mb-5">
             <div class="description">
                 <h5>Description</h5>
-                <hr>
+                <hr class="opacity-25">
                 <p>True-to-life color. Effortless connectivity. Dazzling 4K value. Samsung Crystal UHD is worth a look (and more). Effortlessly access TV shows, movies and ambient content using the Samsung Smart Hub, or find a range of great games on the Samsung Gaming Hub.(High speed internet connection, additional gaming service subscriptions and compatible controller required.) Enjoy content even more clear than it was created as its upgraded to 4K resolution. Color and contrast are also improved with our PurColor and Mega Contrast technologies that instantly analyze and adjust what you see on screen. As all the visual details shine through, you'll be surrounded by 3D sound that moves with the action, engaging all your senses.</p>
             </div>
 
             <div class="specification">
                 <h5>Specification</h5>
-                <p>SKU: VI799EA1QMSQ4NAFAMZ
-                    Model: VP8832DF
-                    Production Country: China
-                    Size (L x W x H cm): -
-                    Weight (kg): 3
-                    Main Material: pvc
-                    Shop Type: Jumia Mall
-                </p>
+                <ul>
+                    <li>SKU: VI799EA1QMSQ4NAFAMZ</li>
+                    <li>Model: VP8832DF</li>
+                    <li>Production Country: China</li>
+                    <li>Size (L x W x H cm): -</li>
+                    <li>Weight (kg): 3</li>
+                    <li>Main Material: pvc</li>
+                    <li>Shop Type: Jumia Mall</li>
+                </ul>
             </div>
 
         </div>
 
         <div class="col-8 round-1 p-3 mb-2 bg-white text-dark border shadow-sm ms-4 mb-5">
-            <div class="col-8">
-                <div class="reviews">
-                    <h5>Customer Reviews</h5>
-                    <hr>
-                    <div class="rating">
-                        <p><span> 5</span> /5 </p>
-                        <div>
-                            <p>5 stars -----------------</p>
-                            <p>4 stars -----------------</p>
-                            <p>3 stars -----------------</p>
-                            <p>2 stars -----------------</p>
-                            <p>1 star ------------------</p>
-                        </div>
-
+            <div class="reviews">
+                <h5>Customer Reviews</h5>
+                <hr>
+                <div class="d-flex flex-row gap-5">
+                    <h3 class="fw-bold "><span class="text-warning"> 5</span> /5 </h3>
+                    <div>
+                        <p>5 stars -----------------</p>
+                        <p>4 stars -----------------</p>
+                        <p>3 stars -----------------</p>
+                        <p>2 stars -----------------</p>
+                        <p>1 star ------------------</p>
                     </div>
-
-
                 </div>
-
             </div>
         </div>
 
         <div class="col-8 rounded-1 p-3 mb-2 bg-white text-dark border shadow-sm ms-4 mb-5">
             <div class="reviews">
                 <h5>Martin M.</h5>
+                <div class="d-flex flex-row gap-row-2 " >
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                </div>
                 <hr>
                 <img src="" alt="">
                 <p>Used fir steaming service quality was excellent I will guarantee it to my friends and family. I am sure they will love it and appreciate it as much as I have enjoyed the experience of being able to try out and enjoying the product. </p>
@@ -94,6 +108,13 @@
         <div class="col-8 rounded-1 p-3 mb-2 bg-white text-dark border shadow-sm ms-4 mb-5">
             <div class="reviews">
                 <h5>Martin M.</h5>
+                <div class="d-flex flex-row gap-row-2 " >
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                </div>
                 <hr>
                 <img src="" alt="">
                 <p>Used fir steaming service quality was excellent I will guarantee it to my friends and family. I am sure they will love it and appreciate it as much as I have enjoyed the experience of being able to try out and enjoying the product. </p>
@@ -101,34 +122,34 @@
         </div>
 
 
-        <div class=" col-8 p-3 mb-2 bg-white text-dark border shadow-sm ms-4">
+        <div class=" col-8 p-3 mb-2 bg-white text-dark border shadow-sm ms-4 rounded-1">
             <h5>You may also like </h5>
             <hr>
-            <div class="likes d-flex justify-content-center grid gap-5 ">
+            <div class="likes d-flex justify-content-center grid gap-4  ">
 
-                <div class="rounded-1 border shadow-sm p-4">
-                    <img src="./p-images/oraimo.jpg" alt="oraimo">
-                    <p>oraimo earpods</p>
-                    <p> KSh. 5,499</p>
-                    <p class="text-decoration-line-through opacity-50"> KSh. 6,499</p>
+                <div class="rounded-1 border shadow-sm p-4 lh-base">
+                    <img src="https://gadgetcentral.co.ke/wp-content/uploads/2022/07/freepods.jpeg" class="w-75  h-50" alt="">
+                    <p>oraimo earpods <br>
+                    <span class="fw-bold" > KSh. 5,499</span> <br>
+                    <span class="text-decoration-line-through opacity-50"> KSh. 6,499</span></p>
                 </div>
-                <div class="rounded-1 border shadow-sm p-4">
-                    <img src="./p-images/sony.jpg" alt="">
-                    <p>oraimo earpods</p>
-                    <p> KSh. 5,499</p>
-                    <p class="text-decoration-line-through opacity-50"> KSh. 6,499</p>
+                <div class="rounded-1 border shadow-sm p-4 lh-base">
+                    <img src="https://gadgetcentral.co.ke/wp-content/uploads/2022/07/freepods.jpeg" class="w-75 h-50" alt="">
+                    <p>oraimo earpods <br>
+                    <span class="fw-bold" > KSh. 5,499</span> <br>
+                    <span class="text-decoration-line-through opacity-50"> KSh. 6,499</span></p>
                 </div>
-                <div class="rounded-1 border shadow-sm p-4">
-                    <img src="" alt="">
-                    <p>oraimo earpods</p>
-                    <p> KSh. 5,499</p>
-                    <p class="text-decoration-line-through opacity-50"> KSh. 6,499</p>
+                <div class="rounded-1 border shadow-sm p-4 lh-base">
+                    <img src="https://gadgetcentral.co.ke/wp-content/uploads/2022/07/freepods.jpeg" class="w-75  h-50" alt="">
+                    <p>oraimo earpods <br>
+                    <span class="fw-bold" > KSh. 5,499</span> <br>
+                    <span class="text-decoration-line-through opacity-50"> KSh. 6,499</span></p>
                 </div>
-                <div class="rounded-1 border shadow-sm p-4">
-                    <img src="" alt="">
-                    <p>oraimo earpods</p>
-                    <p> KSh. 5,499</p>
-                    <p class="text-decoration-line-through opacity-50"> KSh. 6,499</p>
+                <div class="rounded-1 border shadow-sm p-4 lh-base">
+                    <img src="https://gadgetcentral.co.ke/wp-content/uploads/2022/07/freepods.jpeg" class="w-75 h-50" alt="">
+                    <p>oraimo earpods <br>
+                    <span class="fw-bold" > KSh. 5,499</span> <br>
+                    <span class="text-decoration-line-through opacity-50"> KSh. 6,499</span></p>
                 </div>
             </div>
 
